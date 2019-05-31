5c4969239ff1af2902bda8c2bf9d77b4c507989a
noboni <noboni5199@gmail.com>
2019-05-31 03:17:49 -0700

    adding editional endpoint

--- gs-rest-service/complete/src/main/java/hello/GreetingController.java
+++ gs-rest-service/complete/src/main/java/hello/GreetingController.java
@@ -2,10 +2,7 @@ package hello;
 
 import java.util.concurrent.atomic.AtomicLong;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/v1")
@@ -15,7 +12,7 @@ public class GreetingController {
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting/{name}")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+    public Greeting greeting(@PathVariable String name) {
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
     }
